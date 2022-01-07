746b858f84956e723c9205b7bcf2c3440a88b3f8
Yannik Hinteregger <yannik.hinteregger@qaware.de>
2022-01-07 20:39:19 +0100

    implemented mandelbrot calculation (little buggy aber good enough)

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,155 @@
-mod utils;
-
-use std::sync::{Arc, Mutex};
 use std::{sync, thread};
+use std::char::MAX;
+use std::sync::{Arc, mpsc, Mutex};
+use std::sync::mpsc::{Receiver, Sender};
 use std::thread::spawn;
 use std::time::Duration;
+
 use minifb::{Key, Window, WindowOptions};
 use rand::Rng;
 
-const IMG_WIDTH: usize = 500;
-const IMG_HEIGHT: usize = 500;
+use crate::utils::{pixel_color, pixel_to_values, rand_f64};
 
-struct Pixel {
-    idx: usize,
-    color: u32,
+mod utils;
+
+const POS_X: i32 = -2;
+const POS_Y: i32 = -2;
+const HEIGHT: f64 = 2.5;
+
+const IMG_WIDTH: usize = 1023;
+const IMG_HEIGHT: usize = 1023;
+const IMG_RATIO: f64 = IMG_WIDTH as f64 / IMG_HEIGHT as f64;
+
+const NUM_BLOCKS: usize = 128;
+const NUM_THREADS: usize = 10;
+
+const SAMPLES: usize = 100;
+const MAX_ITER: usize = 500;
+
+pub struct Pixel {
+    x: usize,
+    y: usize,
+    r: u8,
+    b: u8,
+    g: u8,
 }
 
-fn white_generator(buffer: Arc<Mutex<Vec<u32>>>) {
-    let (tx, rx) = sync::mpsc::channel();
-    for i in 0..IMG_HEIGHT * IMG_WIDTH {
-        let tx = tx.clone();
-        let i = i.clone();
-        thread::spawn(move || {
-            let duration = rand::thread_rng().gen_range(0..4000);
-            thread::sleep(Duration::from_millis(duration));
-            tx.send(Pixel {
-                idx: i,
-                color: utils::rgb_to_u32(255, 255, 255),
+struct WorkItem {
+    initial_x: usize,
+    final_x: usize,
+    initial_y: usize,
+    final_y: usize,
+}
+
+fn work_item_creator(work_chan: Sender<WorkItem>) {
+    let sqrt = (NUM_BLOCKS as f64).sqrt() as usize;
+    let mut counter = 0;
+    for i in 0..sqrt {
+        for j in 0..sqrt {
+            work_chan.send(WorkItem {
+                initial_x: i * (IMG_WIDTH / sqrt),
+                final_x: (i + 1) * (IMG_WIDTH / sqrt),
+                initial_y: j * (IMG_HEIGHT / sqrt),
+                final_y: (j + 1) * (IMG_HEIGHT / sqrt),
             });
-        });
+            counter += 1;
+            println!("Created item #{}", counter)
+        }
     }
-    println!("done creating");
-    std::mem::drop(tx);
-    for x in rx.iter() {
+}
+
+fn worker_creator(work_rx: Receiver<WorkItem>, result_tx: Sender<Pixel>) {
+    let (status_tx, status_receive) = mpsc::channel();
+    for i in 0..NUM_THREADS {
+        status_tx.send(true);
+    }
+
+    for (num, _) in status_receive.iter().enumerate() {
+        let work_item = work_rx.recv().unwrap();
+        let result_tx = result_tx.clone();
+        let status_tx = status_tx.clone();
+        // println!("Starting worker #{} with item {} {} {} {}", num, work_item.initial_x, work_item.final_x, work_item.initial_y, work_item.final_y);
+        let _ = thread::spawn(move || worker(work_item, result_tx, status_tx));
+    }
+}
+
+fn worker(work_item: WorkItem, result_tx: Sender<Pixel>, status_tx: Sender<bool>) {
+    for x in work_item.initial_x..work_item.final_x {
+        for y in work_item.initial_y..work_item.final_y {
+            let (mut col_r, mut col_g, mut col_b): (u64, u64, u64) = (0, 0, 0);
+            for _ in 0..SAMPLES {
+                let a = HEIGHT as f64 * IMG_RATIO * (((x as f64) + rand_f64()) / (IMG_WIDTH as f64)) + POS_X as f64;
+                let b = HEIGHT as f64 * (((y as f64) + rand_f64()) / (IMG_HEIGHT as f64)) + POS_Y as f64;
+                let (r, iter) = mandelbrot_iteration(a, b);
+                let (r, g, b) = pixel_color(r, iter);
+                col_r += r as u64;
+                col_g += g as u64;
+                col_b += b as u64;
+            }
+            let (cr, cg, cb): (u8, u8, u8);
+            cr = ((col_r as f64) / (SAMPLES as f64)) as u8;
+            cg = ((col_g as f64) / (SAMPLES as f64)) as u8;
+            cb = ((col_b as f64) / (SAMPLES as f64)) as u8;
+
+            result_tx.send(Pixel {
+                x,
+                y,
+                r: cr,
+                b: cb,
+                g: cg,
+            });
+        }
+    }
+    status_tx.send(true);
+}
+
+// fn white_generator(buffer: Arc<Mutex<Vec<u32>>>) {
+//     let (tx, rx) = sync::mpsc::channel();
+//     for i in 0..IMG_HEIGHT * IMG_WIDTH {
+//         let tx = tx.clone();
+//         let i = i.clone();
+//         thread::spawn(move || {
+//             let duration = rand::thread_rng().gen_range(0..4000);
+//             thread::sleep(Duration::from_millis(duration));
+//             tx.send(Pixel);
+//         });
+//     }
+//     println!("done creating");
+//     std::mem::drop(tx);
+//     for x in rx.iter() {
+//         let buffer = buffer.clone();
+//         {
+//             let mut guard = buffer.lock().unwrap();
+//             guard[x.idx] = x.color;
+//         }
+//     }
+// }
+
+fn buffer_updater(buffer: Arc<Mutex<Vec<u32>>>, pixel_receive: Receiver<Pixel>) {
+    for pixel in pixel_receive.iter() {
+        let (idx, color) = pixel_to_values(pixel);
         let buffer = buffer.clone();
         {
             let mut guard = buffer.lock().unwrap();
-            guard[x.idx] = x.color;
+            guard[idx] = color;
         }
     }
+}
+
+fn mandelbrot_iteration(a: f64, b: f64) -> (f64, usize) {
+    let (mut x, mut y, mut xx, mut yy, mut xy): (f64, f64, f64, f64, f64) = (0.0, 0.0, 0.0, 0.0, 0.0);
+    for i in 0..MAX_ITER {
+        xx = x * x;
+        yy = y * y;
+        xy = x * y;
+        if xx + yy > 4.0 {
+            return (xx + yy, i);
+        }
+        x = xx - yy + a;
+        y = 2_f64 * xy + b;
+    }
+
+    (xx + yy, MAX_ITER)
 }
 
 fn main() {
@@ -53,8 +165,14 @@ fn main() {
         panic!("{}", e);
     });
 
-    let test = buffer.clone();
-    let _ = thread::spawn(|| white_generator(test));
+    let (item_send, item_receive) = mpsc::channel();
+    let (result_send, result_receive) = mpsc::channel();
+
+    let _ = thread::spawn(move || work_item_creator(item_send));
+    let _ = thread::spawn(move || worker_creator(item_receive, result_send));
+
+    let buff_update = buffer.clone();
+    let _ = thread::spawn(move || buffer_updater(buff_update, result_receive));
 
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
@@ -69,11 +187,3 @@ fn main() {
             .unwrap();
     }
 }
-
-struct WorkItem {
-    initial_x: i32,
-    final_x: i32,
-    initial_y: i32,
-    final_y: i32,
-}
-
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,70 @@
+use rand::prelude::*;
+
+use crate::{IMG_WIDTH, Pixel};
+
 pub fn rgb_to_u32(r: u8, g: u8, b: u8) -> u32 {
     let (r, g, b) = (r as u32, g as u32, b as u32);
     (r << 16) | (g << 8) | b
+}
+
+pub fn rand_f64() -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen::<f64>()
+}
+
+pub fn pixel_color(r: f64, iter: usize) -> (u8, u8, u8) {
+    if r > 4 as f64 {
+        return hsl_to_rgb((iter as f64) / 100_f64 * r, 1.0, 0.5);
+    }
+    (0, 0, 0) // if in set return black
+}
+
+pub fn hsl_to_rgb(h: f64, s: f64, l: f64) -> (u8, u8, u8) {
+    let (r, g, b): (u8, u8, u8);
+    if s as usize == 0 {
+        r = l as u8;
+        b = l as u8;
+        g = l as u8;
+    } else {
+        let (q, p): (f64, f64);
+        if l < 0.5 {
+            q = l * (1_f64 + s);
+        } else {
+            q = l + s - l * s;
+        }
+        p = 2_f64 * l - q;
+        r = hue_to_color_chan(p, q, h + 1.0 / 3.0);
+        g = hue_to_color_chan(p, q, h);
+        b = hue_to_color_chan(p, q, h - 1.0 / 3.0);
+    }
+    (r, b, g)
+}
+
+pub fn hue_to_color_chan(p: f64, q: f64, mut t: f64) -> u8 {
+    let ret_val: f64;
+    if t < 0_f64 {
+        t += 1_f64
+    }
+    if t > 1_f64 {
+        t -= 1_f64
+    }
+
+    if t < 1.0 / 6.0 {
+        ret_val = p + (q - p) * 6_f64 * t;
+    } else if t < 1.0 / 2.0 {
+        ret_val = q;
+    } else if t < 2.0 / 3.0 {
+        ret_val = p + (q - p) * (2.0 / 3.0 - t) * 6_f64;
+    } else {
+        ret_val = p
+    }
+    (ret_val * 255_f64) as u8
+}
+
+pub fn pixel_to_values(pixel: Pixel) -> (usize, u32) {
+    let idx = IMG_WIDTH * (pixel.y) + pixel.x;
+    let color = rgb_to_u32(pixel.r, pixel.g, pixel.b);
+    // println!("{} {}", pixel.x, pixel.y);
+    // println!("{} {}", idx, color);
+    (idx, color)
 }
