7859681972982f725f568ec23d3f1768cb498b79
Yannik Hinteregger <yannik.hinteregger@qaware.de>
2022-01-08 10:54:31 +0100

    little cleanup and fine-tuning

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,23 @@ use std::time::Duration;
 use minifb::{Key, Window, WindowOptions};
 use rand::Rng;
 
-use crate::utils::{pixel_color, pixel_to_values, rand_f64};
+use crate::utils::{pixel_color, pixel_to_values, rand_f64, rgb_to_u32};
 
 mod utils;
 
-const POS_X: i32 = -2;
-const POS_Y: i32 = -2;
+const SCALE_X: f64 = -2.0;
+const SCALE_Y: f64 = -1.255;
 const HEIGHT: f64 = 2.5;
 
 const IMG_WIDTH: usize = 1023;
 const IMG_HEIGHT: usize = 1023;
 const IMG_RATIO: f64 = IMG_WIDTH as f64 / IMG_HEIGHT as f64;
 
+// chose any number with to the power of two => n^2 = NUM_BLOCKS
 const NUM_BLOCKS: usize = 128;
 const NUM_THREADS: usize = 10;
 
-const SAMPLES: usize = 100;
+const SAMPLES: usize = 200;
 const MAX_ITER: usize = 500;
 
 pub struct Pixel {
@@ -43,7 +44,6 @@ struct WorkItem {
 
 fn work_item_creator(work_chan: Sender<WorkItem>) {
     let sqrt = (NUM_BLOCKS as f64).sqrt() as usize;
-    let mut counter = 0;
     for i in 0..sqrt {
         for j in 0..sqrt {
             work_chan.send(WorkItem {
@@ -52,24 +52,22 @@ fn work_item_creator(work_chan: Sender<WorkItem>) {
                 initial_y: j * (IMG_HEIGHT / sqrt),
                 final_y: (j + 1) * (IMG_HEIGHT / sqrt),
             });
-            counter += 1;
-            println!("Created item #{}", counter)
         }
     }
 }
 
 fn worker_creator(work_rx: Receiver<WorkItem>, result_tx: Sender<Pixel>) {
     let (status_tx, status_receive) = mpsc::channel();
-    for i in 0..NUM_THREADS {
+    for _ in 0..NUM_THREADS {
         status_tx.send(true);
     }
 
-    for (num, _) in status_receive.iter().enumerate() {
-        let work_item = work_rx.recv().unwrap();
+    for _ in status_receive.iter() {
+        let work_item = work_rx.recv();
+        if work_item.is_err() { return; }
         let result_tx = result_tx.clone();
         let status_tx = status_tx.clone();
-        // println!("Starting worker #{} with item {} {} {} {}", num, work_item.initial_x, work_item.final_x, work_item.initial_y, work_item.final_y);
-        let _ = thread::spawn(move || worker(work_item, result_tx, status_tx));
+        let _ = thread::spawn(move || worker(work_item.unwrap(), result_tx, status_tx));
     }
 }
 
@@ -78,8 +76,8 @@ fn worker(work_item: WorkItem, result_tx: Sender<Pixel>, status_tx: Sender<bool>
         for y in work_item.initial_y..work_item.final_y {
             let (mut col_r, mut col_g, mut col_b): (u64, u64, u64) = (0, 0, 0);
             for _ in 0..SAMPLES {
-                let a = HEIGHT as f64 * IMG_RATIO * (((x as f64) + rand_f64()) / (IMG_WIDTH as f64)) + POS_X as f64;
-                let b = HEIGHT as f64 * (((y as f64) + rand_f64()) / (IMG_HEIGHT as f64)) + POS_Y as f64;
+                let a = HEIGHT as f64 * IMG_RATIO * (((x as f64) + rand_f64()) / (IMG_WIDTH as f64)) + SCALE_X;
+                let b = HEIGHT as f64 * (((y as f64) + rand_f64()) / (IMG_HEIGHT as f64)) + SCALE_Y;
                 let (r, iter) = mandelbrot_iteration(a, b);
                 let (r, g, b) = pixel_color(r, iter);
                 col_r += r as u64;
@@ -102,28 +100,6 @@ fn worker(work_item: WorkItem, result_tx: Sender<Pixel>, status_tx: Sender<bool>
     }
     status_tx.send(true);
 }
-
-// fn white_generator(buffer: Arc<Mutex<Vec<u32>>>) {
-//     let (tx, rx) = sync::mpsc::channel();
-//     for i in 0..IMG_HEIGHT * IMG_WIDTH {
-//         let tx = tx.clone();
-//         let i = i.clone();
-//         thread::spawn(move || {
-//             let duration = rand::thread_rng().gen_range(0..4000);
-//             thread::sleep(Duration::from_millis(duration));
-//             tx.send(Pixel);
-//         });
-//     }
-//     println!("done creating");
-//     std::mem::drop(tx);
-//     for x in rx.iter() {
-//         let buffer = buffer.clone();
-//         {
-//             let mut guard = buffer.lock().unwrap();
-//             guard[x.idx] = x.color;
-//         }
-//     }
-// }
 
 fn buffer_updater(buffer: Arc<Mutex<Vec<u32>>>, pixel_receive: Receiver<Pixel>) {
     for pixel in pixel_receive.iter() {
@@ -153,7 +129,7 @@ fn mandelbrot_iteration(a: f64, b: f64) -> (f64, usize) {
 }
 
 fn main() {
-    let mut buff: Vec<u32> = vec![0; IMG_WIDTH * IMG_HEIGHT];
+    let mut buff: Vec<u32> = vec![rgb_to_u32(255, 255, 255); IMG_WIDTH * IMG_HEIGHT];
     let buffer = Arc::new(Mutex::new(buff));
 
     let mut window = Window::new(
